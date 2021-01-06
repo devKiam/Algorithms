ad52c295e9637f58a3fff10cdc5d611f8dee0df3
Md. Nuho-Ul Alam Kiam <mdnuhoulalam@gmail.com>
2021-01-06 15:22:40 +0600

    Binary Search | Left most greatest, right most smallest

--- Binary search.cpp
+++ Binary search.cpp
@@ -17,6 +17,32 @@ bool binarySearch(vector<int> &v, int val)
 }
 
 
+int binarySearch_Right_MostSmallest(vector<int> &v, int val)
+{
+    int lo = 0, hi = v.size()-1, mid;
+    while(lo < hi){
+        mid = (lo + hi + 1) / 2;
+        if(val >= v[mid]) lo = mid;
+        else hi = mid - 1;
+    }
+    if(v[lo] > val) return lo;
+    else return lo + 1;
+}
+
+
+int binarySearch_Left_MostGreatest(vector<int> &v, int val)
+{
+    int lo = 0, hi = v.size()-1, mid;
+    while(lo < hi){
+        mid = (lo + hi) / 2;
+        if(val <= v[mid]) hi = mid;
+        else lo = mid + 1;
+    }
+    if(v[lo] < val) return hi;
+    else return hi - 1;
+}
+
+
 int main()
 {
     ios_base::sync_with_stdio(false); cin.tie(nullptr);
@@ -28,5 +54,12 @@ int main()
     }
     cout<<binary_search(v.begin(), v.end(), 8)<<endl; // stl built-in function
     cout<<binarySearch(v, 7)<<endl; // my implementation
+    
+    
+    int q; cin>>q;
+    int smaller = binarySearch_Left_MostGreatest(v, q);
+    int greater = binarySearch_Right_MostSmallest(v, q);
+    if(smaller > -1 && smaller < v.size()) cout<<v[smaller]<<" ";
+    if(greater > -1 && greater < v.size()) cout<<v[greater]<<endl;
 
 }
